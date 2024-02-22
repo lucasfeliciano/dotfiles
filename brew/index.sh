ae162fa0087350f6680918316d3d2bd7278bd02e
Lucas Feliciano <mail@lucasfeliciano.com>
2024-02-22 02:29:58 +0100

    feat: install homebrew

---
+++ brew/index.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+source ../utils/utils.sh
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv)"

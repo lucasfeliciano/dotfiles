d453822d549b13fb8164b012a818d3d92a99e860
Lucas Feliciano <mail@lucasfeliciano.com>
2024-03-29 12:36:20 +0100

    feat: add zsh syntax highlight plugin

--- oh-my-zsh/index.sh
+++ oh-my-zsh/index.sh
@@ -10,6 +10,13 @@ else
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
 
+# Install zsh-syntax-highlighting
+if [ -d "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting" ]; then
+  e_warning "zsh-syntax-highlighting is already installed. skipping.."
+else
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+fi
+
 # Install spaceship theme
 if [ -d "$ZSH_CUSTOM/themes/spaceship-prompt" ]; then
   e_warning "Spaceship theme is already installed. skipping.."

30cca4983a9884d93f9a15427f10fc1cceec4445
Lucas Feliciano <mail@lucasfeliciano.com>
2026-03-31 23:12:39 +0200

    fix: use built-in Catppuccin Latte theme for Ghostty
    Remove custom theme file and symlink in favor of Ghostty's bundled theme.

--- lib/ghostty.sh
+++ lib/ghostty.sh
@@ -5,5 +5,4 @@ setup_ghostty() {
   run mkdir -p ~/.config/ghostty
 
   run ln -sf "$DOTFILES_DIR/config/ghostty/config" ~/.config/ghostty/config
-  run ln -sf "$DOTFILES_DIR/config/ghostty/themes" ~/.config/ghostty/themes
 }
